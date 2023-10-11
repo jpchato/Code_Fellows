0bef332772b9d90e9990f34a73ef217e857ff588
jpchato <jpchato@gmail.com>
2023-10-10 17:00:48 -0700

    first commit, array reverse complete

---
+++ data-structures-and-algorithms/array_reverse.py
@@ -0,0 +1,15 @@
+"""
+Feature Tasks
+Write a function called reverseArray which takes an array as an argument. Without utilizing any of the built-in methods available to your language, return an array with elements in reversed order.
+Example
+Input	Output
+[1, 2, 3, 4, 5, 6]	[6, 5, 4, 3, 2, 1]
+[89, 2354, 3546, 23, 10, -923, 823, -12]	[-12, 823, -923, 10, 23, 3546, 2354, 89]
+
+"""
+
+def array_reverse(input_list):
+    return input_list[::-1]
+
+print(array_reverse([1, 2, 3, 4, 5, 6]))
+print(array_reverse([89, 2354, 3546, 23, 10, -923, 823, -12]))
